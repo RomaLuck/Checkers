7b92102e8fddf1d57d6485e51963fac0c551b32e
RomaLuck <romeo91222@gmail.com>
2024-05-25 13:07:30 +0200

    convert input data

--- app/controllers/start.php
+++ app/controllers/start.php
@@ -1,8 +1,8 @@
 <?php
 
 if (isset($_POST['white'], $_POST['black'])) {
-    $_SESSION['white'] = $_POST['white'];
-    $_SESSION['black'] = $_POST['black'];
+    $_SESSION['white'] = htmlspecialchars($_POST['white']);
+    $_SESSION['black'] = htmlspecialchars($_POST['black']);
 
     redirect('/game');
 }
